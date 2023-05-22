fc746536240fc1be33c45212977f7eca5bf9ab73
Karton <116901073+Kartonowy@users.noreply.github.com>
2023-05-22 13:46:22 +0200

    it just works

--- arduino/sketch_may20a/sketch_may20a.ino
+++ arduino/sketch_may20a/sketch_may20a.ino
@@ -1,6 +1,26 @@
 const int PIN_RED = 5;
 const int PIN_GREEN = 6;
 const int PIN_BLUE = 9;
+String pyinput;
+
+uint8_t RGB[3];
+
+void hexToRGBArray(const String& hex, uint8_t rgb[]) {
+  if (hex.length() != 6) {
+    // Invalid hexadecimal color code
+    return;
+  }
+
+  for (int i = 0; i < 3; i++) {
+    uint8_t highNibble = hex.charAt(i * 2);
+    uint8_t lowNibble = hex.charAt(i * 2 + 1);
+
+    uint8_t highByte = (highNibble >= '0' && highNibble <= '9') ? (highNibble - '0') : (highNibble - 'A' + 10);
+    uint8_t lowByte = (lowNibble >= '0' && lowNibble <= '9') ? (lowNibble - '0') : (lowNibble - 'A' + 10);
+
+    rgb[i] = (highByte << 4) | lowByte;
+  }
+}
 
 void setup() {
   // put your setup code here, to run once:  
@@ -15,12 +35,19 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  int RGB[3] = {251, 0, 255};
+  while (Serial.available() == 0) {}
+
+  pyinput = Serial.readStringUntil('\r');
+
+  hexToRGBArray(pyinput, RGB);
+
+  Serial.println(pyinput);
+
   setColor(RGB);
 
 }
 
-void setColor(int RGB[]) {
+void setColor(uint8_t RGB[]) {
   analogWrite(PIN_RED, RGB[0]);
   analogWrite(PIN_GREEN, RGB[1]);
   analogWrite(PIN_BLUE, RGB[2]);
--- controller/main.py
+++ controller/main.py
@@ -1,24 +1,28 @@
 from PIL import ImageGrab
-import matplotlib as mpl
+from serial import Serial
+import time
 
-def get_color(pil_img):
-    img = pil_img.copy()
+arduino = Serial("com6", 9600)
+
+
+def get_color(img):
     img = img.convert("RGBA")
-    img = img.resize((1, 1), resample = 0)
-    dominant_color = img.getpixel((0,0))
+    img = img.resize((1, 1), resample=0)
+    dominant_color = img.getpixel((0, 0))
     return dominant_color
+
 
 run = True
 
-while (run == True):
-    image = ImageGrab.grab()
-    print('#%02x%02x%02x%02x' % get_color(image))
-
-
-    
-# import serial
-
-# a = 255
-# arduino = serial.Serial('com6', 9600)
-
-# arduino.write(a.encode())
+while run:
+    try:
+        image = ImageGrab.grab()
+        pyinput = "%02x%02x%02x%02x" % get_color(image)
+        pyinput = pyinput[:6] + "\r"
+        print(pyinput)
+        arduino.write(pyinput.encode())
+        image.close()
+        time.sleep(0.1)
+    except KeyboardInterrupt:
+        run = False
+        arduino.close()
